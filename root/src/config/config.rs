88f6693deed69db41055e23e7e88190f18982c68
sarthak7awasthi <sa3655@drexel.edu>
2025-01-17 00:53:34 -0500

    config added

---
+++ root/src/config/config.rs
@@ -0,0 +1,8 @@
+pub const MAX_SEQ_LENGTH: usize = 128;
+pub const BATCH_SIZE: usize = 32;     
+
+
+pub const PAD_TOKEN: &str = "[PAD]";
+pub const UNK_TOKEN: &str = "[UNK]";
+pub const CLS_TOKEN: &str = "[CLS]";
+pub const SEP_TOKEN: &str = "[SEP]";
